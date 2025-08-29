3a22027c6f2b0a30402d9da98df3889bdff8cb64
unknown <CETECOM@AOAWSB03LC0908.duoc.acad>
2025-08-29 11:22:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mip/MainActivity2.kt
@@ -0,0 +1,40 @@
+package com.example.mip
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        val txtUsuario: TextView = findViewById(R.id.id_usuario_logeado)
+        val recibirUsernameS = intent.getStringExtra("par_userr")
+
+        txtUsuario.text = recibirUsernameS.toString()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
