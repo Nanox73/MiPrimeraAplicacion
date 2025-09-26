baeecbb7dd703dc20f2c237462bb4066ac585014
Nanox73 <fe.aguila@duocuc.cl>
2025-09-26 11:50:45 -0300

    activity6 xd lol

---
+++ app/src/main/java/com/example/mip/MainActivity6.kt
@@ -0,0 +1,51 @@
+package com.example.mip
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.reflect.jvm.internal.impl.descriptors.PropertySetterDescriptor
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main6)
+
+        val listvIew: ListView = findViewById(R.id.ls_vw)
+
+        val opcionesArr = arrayOf(
+            "IR A CALCULADORA"
+            , "IR MENU"
+            , "Spinner rest api"
+        )
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, opcionesArr)
+
+        listvIew.adapter = adaptador
+
+        listvIew.setOnItemClickListener { parent, view, position, id ->
+            val itemElegido = parent.getItemAtPosition(position).toString()
+            if(itemElegido == "IR A CALCULADORA"){
+                val nuevaVentan = Intent
+            }
+            Toast.makeText(this, itemElegido, Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
